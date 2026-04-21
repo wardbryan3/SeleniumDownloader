cdb49fa17078817e063c9994f365eeb68e7576a4
Bryan Ward <wardbryan3@gmail.com>
2026-04-21 11:35:14 -0700

    Add constants.py and ignore build/dist artifacts

---
+++ constants.py
@@ -0,0 +1,9 @@
+"""
+Shared constants for download handling
+"""
+
+ALLOWED_EXTENSIONS = {'.zip', '.mp3', '.wav', '.pdf', '.rar', '.7z', '.tar', '.gz'}
+
+EXCLUDED_EXTENSIONS = {'.part', '.crdownload', '.tmp', '.download', '.xpi', '.so', '.lock'}
+
+EXCLUDED_PREFIXES = {'.fea', '.X'}
